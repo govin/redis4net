a328042ed6e2235237d6c3b83be531eee130707a
Govind <grajagopalan@opentable.com>
2014-02-18 08:24:16 -0800

    update usage.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("redis4net")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]	
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: ComVisible(false)]	
+
