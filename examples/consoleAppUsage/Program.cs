5241071077c8192f2226e46ccddf5890edbfbf12
Govind <grajagopalan@opentable.com>
2014-02-18 08:16:56 -0800

    usage examples first check in.

---
+++ examples/consoleAppUsage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redis4netUsage
+{
+	using System.Net.Mime;
+
+	using log4net;
+	using log4net.Config;
+
+	class Program
+	{
+		private static readonly ILog logger = LogManager.GetLogger(typeof(Program).Name);
+
+		static Program()
+		{
+			XmlConfigurator.Configure();
+		}
+
+		static void Main(string[] args)
+		{
+			logger.Info("Hello world from redis appender");
+			var dict = new Dictionary<string, string>();
+			dict.Add("mykey", "myvalue");
+			dict.Add("mykey2", "myvalue2");
+			dict.Add("Message", "my message hello world");
+			logger.Info(dict);
+			logger.Error(dict, new ArgumentException("Argument exception encountered"));
+		}
+	}
+}
