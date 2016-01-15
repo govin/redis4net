f13ee1986cbfb4a050b134aedf693e4a96423a4d
Patrik <rambo3swe@gmail.com>
2016-01-15 08:49:25 +0100

    [FEATURE] impelemented so redis4net can use clusterconnectionstrings

--- src/redis4net/Redis/IConnection.cs
+++ src/redis4net/Redis/IConnection.cs
@@ -4,6 +4,7 @@ namespace redis4net.Redis
 {
 	public interface IConnection
 	{
+	    void OpenCluster(string clusterConnectionString, string listName);
 		void Open(string hostname, int port, string listName);
 		bool IsOpen();
 		Task<long> AddToList(string content);
