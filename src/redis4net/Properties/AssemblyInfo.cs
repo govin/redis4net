e14e519661ef52e6b61d4f0afe36f6644f277182
saranshparashar <saransh.parashar@blackbaud.me>
2022-01-25 17:31:54 +0530

    Nuget Changes

--- src/redis4net/Properties/AssemblyInfo.cs
+++ src/redis4net/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("gelf4net")]
-[assembly: AssemblyProduct("gelf4net")]
+[assembly: AssemblyTitle("Redis4Net")]
+[assembly: AssemblyProduct("Redis4net")]
 [assembly: Guid("C92B1448-EF37-432B-B9C2-C843831B3CFA")]
+[assembly: AssemblyDescription("redis4net is a log4net appender that formats logs and adds them to redis lists")]
